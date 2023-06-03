a93d51076b4cda440eab9437d78e0f21fe3a1b0d
Tomiloba <ayoloba8@gmail.com>
2023-06-03 23:04:14 +0100

    ok

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const paragraph = document.querySelector('#readmore');
-const showMoreBtn = document.querySelector('#showMoreBtn');
-const divs = document.querySelector('.days')
-// Check if the <p> tag and button exist
+const paragraphs = document.querySelectorAll('.readmore');
+const showMoreBtns = document.querySelectorAll('.showMoreBtn');
 
-
-
-if (paragraph && showMoreBtn) {
+// Check if the <p> tags and buttons exist
+if (paragraphs.length === showMoreBtns.length) {
+  paragraphs.forEach((paragraph, index) => {
+    const showMoreBtn = showMoreBtns[index];
+    
     // Get the text content of the <p> tag
     const text = paragraph.textContent.trim();
     
-    
     // Check the length of the text
     if (text.length > 30) {
-        const trimmedText = text.slice(0, 100);
-        const hiddenWords = text.slice(100);
-    
-        // Update the <p> tag with trimmed text
-        paragraph.textContent = trimmedText;
-    
-        // Set onclick event for the button
-        showMoreBtn.onclick = function() {
+      const trimmedText = text.slice(0, 100);
+      const hiddenWords = text.slice(100);
+  
+      // Update the <p> tag with trimmed text
+      paragraph.textContent = trimmedText;
+  
+      // Set onclick event for the button
+      showMoreBtn.onclick = function() {
         // Append the hidden words when the button is clicked
         // paragraph.textContent += hiddenWords;
         // Remove the button after showing the hidden words
         showMoreBtn.remove();
-        
-        };
+      };
     }
+  });
 } else {
-        
-    console.log('The <p> tag or button does not exist.');
+  console.log('The number of <p> tags and buttons does not match.');
 }
-    // }
-    
     
 
 const toggleButton=document.getElementsByClassName('toggle-button')[0]
